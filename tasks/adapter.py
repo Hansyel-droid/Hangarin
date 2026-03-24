7d9b5d1526e6a4a3c5ecdce62c7ef1ea40d7d30b
Hansyel_droid <hansmagalaman123@gmail.com>
2026-03-24 19:05:01 +0800

    update the adapter for admin

--- tasks/adapter.py
+++ tasks/adapter.py
@@ -1,18 +1,15 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from django.core.exceptions import PermissionDenied
 
 
 class CorporateOnlyAdapter(DefaultSocialAccountAdapter):
-    """
-    Allows any Google account to log in.
-    To restrict to a specific domain, uncomment and edit the block below.
-    """
 
     def is_open_for_signup(self, request, sociallogin):
         return True
 
-    # Uncomment below to restrict to a specific email domain:
-    # def pre_social_login(self, request, sociallogin):
-    #     email = sociallogin.account.extra_data.get('email', '')
-    #     if not email.endswith('@yourdomain.com'):
-    #         raise PermissionDenied("Only corporate accounts are allowed.")
+    def save_user(self, request, sociallogin, form=None):
+        user = super().save_user(request, sociallogin, form)
+        # Ensure Google login users are NEVER staff or superusers
+        user.is_staff = False
+        user.is_superuser = False
+        user.save()
+        return user
