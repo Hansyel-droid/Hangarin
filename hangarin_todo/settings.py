68a7283a4c6814cd1e0a6987a0e54e2126109494
Hansyel_droid <hansmagalaman123@gmail.com>
2026-04-07 11:22:46 +0000

    fix the id again

--- hangarin_todo/settings.py
+++ hangarin_todo/settings.py
@@ -94,9 +94,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if "pythonanywhere" in socket.gethostname():
-    SITE_ID = 2  # Keep this for your live site
+    SITE_ID = 2  # ← pythonanywhere.com
 else:
-    SITE_ID = 1  # Match this to your local Admin URL
+    SITE_ID = 3  # ← 127.0.0.1:8000
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
